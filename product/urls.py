cb7ea7bd9850865846611c7f52c0139e03fded36
nurlan5t <nur23kg@mail.ru>
2022-06-16 18:20:57 +0600

    created: orders history api

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,7 @@ from product.views import CollectionsListView, ProductDetailView, \
     CollectionDetailView, ProductsNoveltiesView, ProductsListView, \
     ProductLikeView, ProductsFavoritesView, FiveRandomProducts, \
     ProductsCartView, ProductCartView, CartCreateView, order_info_view,\
-    OrderCreateView, ProductsBestsellersView
+    OrderCreateView, ProductsBestsellersView, OrdersHistoryView
 
 
 urlpatterns = [
@@ -23,6 +23,7 @@ urlpatterns = [
     path('cart/', ProductsCartView.as_view(), name='cart_list'),
     path('cart/<int:pk>/', ProductCartView.as_view(), name='cart_detail'),
     path('products/add_to_cart/', CartCreateView.as_view()),
-    path('order/', order_info_view),
+    path('order/total_sum/', order_info_view),
     path('order/create/', OrderCreateView.as_view()),
+    path('order/history/', OrdersHistoryView.as_view()),
 ]
